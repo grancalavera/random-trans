24d31ad109115a20eca9eb8266fd61fea98b6399
Leon Coto <leoncoto@gmail.com>
2019-04-22 10:32:06 +0100

    Shuffle and select randomly from list

---
+++ src/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import           Prelude                           hiding ( (!!) )
+import           System.Random
+import           Data.List.NonEmpty                       ( NonEmpty(..)
+                                                          , (!!)
+                                                          , fromList
+                                                          )
+import           Control.Monad.Random.Class        hiding ( fromList )
+import           Control.Monad.Random.Strict       hiding ( fromList )
+
+main :: IO ()
+main = do
+  print =<< evalRandIO (shuffle ([] :: [Int]))
+  print =<< evalRandIO (shuffle [1])
+  print =<< evalRandIO (shuffle [1 .. 10])
+
+shuffle :: (RandomGen g, Eq a) => [a] -> Rand g [a]
+shuffle [] = return []
+shuffle xs = do
+  x'  <- choose $ fromList xs
+  xs' <- shuffle $ filter (x' /=) xs
+  return $ x' : xs'
+
+choose :: (RandomGen g) => NonEmpty a -> Rand g a
+choose xs = (xs !!) <$> getRandomR (0, length xs - 1)
