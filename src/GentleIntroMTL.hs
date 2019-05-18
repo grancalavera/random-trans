2fcbeaf3c22cb7a3115ac8f3f154bbcc6183a2b6
Leon Coto <leoncoto@gmail.com>
2019-05-18 12:16:48 +0100

    add MonadIO constraint

--- src/GentleIntroMTL.hs
+++ src/GentleIntroMTL.hs
@@ -9,6 +9,9 @@ import qualified Data.Text.IO                  as T
 import qualified Data.Map                      as Map
 import           Data.Map                                 ( Map )
 import           Control.Applicative                      ( liftA2 )
+import           Control.Monad.IO.Class                   ( MonadIO
+                                                          , liftIO
+                                                          )
 import           Control.Monad.Trans                      ( lift )
 import           Control.Monad.Except                     ( ExceptT
                                                           , catchError
@@ -29,37 +32,37 @@ data LoginError = InvalidEmail
 users :: Map Text Text
 users = Map.fromList [("example.com", "qwerty123"), ("localhost", "password")]
 
-loginDialogue :: ExceptT LoginError IO ()
+loginDialogue :: MonadIO m => ExceptT LoginError m ()
 loginDialogue = do
   let retry = userLogin `catchError` wrongPasswordHandler
   token <- retry `catchError` printError
-  lift $ T.putStrLn ("Logged in with token: " <> token)
+  liftIO $ T.putStrLn ("Logged in with token: " <> token)
 
-wrongPasswordHandler :: LoginError -> ExceptT LoginError IO Text
+wrongPasswordHandler :: MonadIO m => LoginError -> ExceptT LoginError m Text
 wrongPasswordHandler WrongPassword = do
-  lift $ T.putStrLn "Wrong password, one more chance"
+  liftIO $ T.putStrLn "Wrong password, one more chance"
   userLogin
 wrongPasswordHandler err = throwError err
 
-printError :: LoginError -> ExceptT LoginError IO a
+userLogin :: MonadIO m => ExceptT LoginError m Text
+userLogin = do
+  token    <- getToken
+  userpw   <- maybe (throwError NoSuchUser) return (Map.lookup token users)
+  password <- liftIO (T.putStrLn "Enter your password:" >> T.getLine)
+  if userpw == password then return token else throwError WrongPassword
+
+printError :: MonadIO m => LoginError -> ExceptT LoginError m a
 printError err = do
-  lift . T.putStrLn $ case err of
+  liftIO . T.putStrLn $ case err of
     WrongPassword -> "Wrong password, no more chances"
     NoSuchUser    -> "No user with that email exists"
     InvalidEmail  -> "Invalid email address entered"
   throwError err
 
-userLogin :: ExceptT LoginError IO Text
-userLogin = do
-  token    <- getToken
-  userpw   <- maybe (throwError NoSuchUser) return (Map.lookup token users)
-  password <- lift (T.putStrLn "Enter your password:" >> T.getLine)
-  if userpw == password then return token else throwError WrongPassword
-
-getToken :: ExceptT LoginError IO Text
+getToken :: MonadIO m => ExceptT LoginError m Text
 getToken = do
-  lift (T.putStrLn "Enter email address")
-  input <- lift T.getLine
+  liftIO (T.putStrLn "Enter email address")
+  input <- liftIO T.getLine
   liftEither (getDomain input)
 
 getDomain :: Text -> Either LoginError Text
